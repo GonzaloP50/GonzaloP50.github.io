5812a4a10204a63f922d35f25ba662e8a88e0a43
Gonzalo Puigcernau <gonzalopuigcernau@gmail.com>
2023-02-16 16:40:17 -0300

    ultimo del 16/2

---
+++ funciones.js
@@ -0,0 +1,5 @@
+document.getElementById("celda1").onclick=function(){
+	//document.getElementById("1").innerHTML="estudié un monton gil";
+	document.getElementById("1").style.display="none";
+	document.getElementById("3").style.display="block";
+}
