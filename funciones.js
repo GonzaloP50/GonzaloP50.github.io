2e8b5e749eb92d58177d6c44a3c144a332862445
Gonzalo Puigcernau <gonzalopuigcernau@gmail.com>
2023-02-21 18:55:37 -0300

    anduvo el pop up

--- funciones.js
+++ funciones.js
@@ -1,5 +1,25 @@
 document.getElementById("celda1").onclick=function(){
-	//document.getElementById("1").innerHTML="estudié un monton gil";
-	document.getElementById("1").style.display="none";
-	document.getElementById("3").style.display="block";
+	document.getElementById("VentanaTel").style.display="block";
+	document.getElementById("VentanaTel").style.backgroundColor="yellow";
+}
+
+document.getElementById("CerrarTel").onclick=function(){
+	document.getElementById("VentanaTel").style.display="none";
+}
+
+document.getElementById("celda2").onclick=function(){
+	document.getElementById("VentanaMail").style.display="block";
+	document.getElementById("VentanaMail").style.backgroundColor="yellow";
+}
+
+document.getElementById("CerrarMail").onclick=function(){
+	document.getElementById("VentanaMail").style.display="none";
+}
+
+document.getElementById("celda3").onclick=function(){
+	document.getElementById("VentanaEstudios").style.display="block";
+}
+
+document.getElementById("CerrarEstudios").onclick=function(){
+	document.getElementById("VentanaEstudios").style.display="none";
 }
